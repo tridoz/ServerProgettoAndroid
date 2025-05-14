1a808f2171666c2923064ca36512e5dee3a3f884
tridoz <149873185+tridoz@users.noreply.github.com>
2025-05-14 19:10:45 +0200

    Indented Database_Connection.h

--- Database_Connection.h
+++ Database_Connection.h
@@ -28,31 +28,31 @@ private:
 public:
     Database_Connection();
 
-    int find_user_id(std::string username);
-    int find_playlist_id(std::string playlist_name);
-    int find_song_id(std::string song_name);
+    int find_user_id( std::string username );
+    int find_playlist_id( std::string playlist_name );
+    int find_song_id( std::string song_name );
 
-    int check_if_user_exists(std::string username);
-    int check_credentials(std::string username, std::string password);
-    int check_if_token_valid(std::string username, std::string token, int token_type);
-    int check_token_generation(std::string accesso_token, std::string refresh_token, std::string revoke_token);
-    int get_playlist_content(std::string playlist_name);
-    int search_songs(std::string song_name);
-    int search_playlists(std::string playlist_name);
-    int search_artists(std::string artist_name);
-    int search_users(std::string user_name);
-    int search_song_in_playlist(std::string username, std::string playlist_name, std::string song_name);
+    int check_if_user_exists( std::string username );
+    int check_credentials( std::string username, std::string password );
+    int check_if_token_valid( std::string username, std::string token, int token_type );
+    int check_token_generation( std::string accesso_token, std::string refresh_token, std::string revoke_token );
+    int get_playlist_content( std::string playlist_name );
+    int search_songs( std::string song_name );
+    int search_playlists( std::string playlist_name );
+    int search_artists( std::string artist_name );
+    int search_users( std::string user_name );
+    int search_song_in_playlist( std::string username, std::string playlist_name, std::string song_name );
 
-    std::string get_user_playlist(int user_id, std::string username);
+    std::string get_user_playlist( int user_id, std::string username );
 
-    int add_user(std::string username, std::string password);
-    int add_token(std::string access_token, std::string refresh_token, std::string revoke_token, std::string username);
-    int add_song_to_playlist(std::string playlist, std::string song_name, std::string username );
+    int add_user( std::string username, std::string password );
+    int add_token( std::string access_token, std::string refresh_token, std::string revoke_token, std::string username );
+    int add_song_to_playlist( std::string playlist, std::string song_name, std::string username );
     int add_song_to_liked( std::string username, std::string song_name );
-    int create_playlist(std::string username, std::string playlist_name, bool isPrivate);
+    int create_playlist( std::string username, std::string playlist_name, bool isPrivate );
 
-    int delete_token(std::string access_token, std::string refresh_token, std::string revoke_token);
-    int delete_user_token(std::string username);
+    int delete_token( std::string access_token, std::string refresh_token, std::string revoke_token) ;
+    int delete_user_token( std::string username );
 };
 
 
