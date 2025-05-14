20f8124a62b54a5b660f16bf29c7684974208b8a
tridoz <149873185+tridoz@users.noreply.github.com>
2025-05-14 19:09:18 +0200

    Indented main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "TCP_Server.h"
 
 
-int main(int argc, char *argv[]) {
+int main( int argc, char *argv[] ) {
     //Database_Connection db_conn;
     TCP_Server server;
     server.Start();
